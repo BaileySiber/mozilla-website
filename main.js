e8dd9b30a2f8c05de77ce4fb357f2f34cc2edd72
baileysiber <bsiber@hks.harvard.edu>
2023-01-06 13:55:49 -0500

    added single button to toggle fox image on and off

--- main.js
+++ main.js
@@ -2,33 +2,48 @@ const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
 
 // Define a Function when fox is clicked
- function sayOuch() {
- 	alert('Ouch! Stop poking me!');
- }
+function sayOuch() {
+  alert('Ouch! Stop poking me!');
+}
 
- //Define a function to hide fox when button one is clicked
- function hideFox() {
- 	foxImage.style.visibility = 'hidden';
- }
+//Define a function to hide fox when button one is clicked
+function hideFox() {
+  foxImage.style.visibility = 'hidden';
+}
 
 function showFox() {
   foxImage.style.visibility = 'visible';
 }
 
- // Use a CSS selector to identify an element
- var foxImage = document.querySelector('img');
+function toggleFox() {
+  if(foxImage.style.visibility == 'visible') {
+    foxImage.style.visibility = 'hidden'
+  }
+  else {
+    foxImage.style.visibility = 'visible'
+  }
+}
 
- // Assign the function to the onclick event on that element
- foxImage.onclick = sayOuch;
+// Use a CSS selector to identify an element
+var foxImage = document.querySelector('img');
 
-// Use CSS ID to identify button one
- var buttonOne = document.getElementById('buttonOne')
+// Assign the function to the onclick event on that element
+foxImage.onclick = sayOuch;
 
-// assign function hide fox when button one clicked
- buttonOne.onclick = hideFox;
+// // Use CSS ID to identify button one
+// var buttonOne = document.getElementById('buttonOne')
+//
+// // assign function hide fox when button one clicked
+// buttonOne.onclick = hideFox;
+//
+// // use css ID to identify button two
+// var buttonTwo = document.getElementById('buttonTwo')
+//
+// // assign function show fox when button two clicked
+// buttonTwo.onclick = showFox;
 
 // use css ID to identify button two
- var buttonTwo = document.getElementById('buttonTwo')
+var buttonThree = document.getElementById('buttonThree')
 
 // assign function show fox when button two clicked
- buttonTwo.onclick = showFox;
+buttonThree.onclick = toggleFox;

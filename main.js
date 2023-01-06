12bd5415a1f9f6508008fc7ae8e881c6f04e7915
baileysiber <bsiber@hks.harvard.edu>
2023-01-06 13:47:57 -0500

    added show and hide fox buttons

--- main.js
+++ main.js
@@ -6,8 +6,29 @@ myHeading.textContent = "Hello world!";
  	alert('Ouch! Stop poking me!');
  }
 
+ //Define a function to hide fox when button one is clicked
+ function hideFox() {
+ 	foxImage.style.visibility = 'hidden';
+ }
+
+function showFox() {
+  foxImage.style.visibility = 'visible';
+}
+
  // Use a CSS selector to identify an element
  var foxImage = document.querySelector('img');
 
  // Assign the function to the onclick event on that element
  foxImage.onclick = sayOuch;
+
+// Use CSS ID to identify button one
+ var buttonOne = document.getElementById('buttonOne')
+
+// assign function hide fox when button one clicked
+ buttonOne.onclick = hideFox;
+
+// use css ID to identify button two
+ var buttonTwo = document.getElementById('buttonTwo')
+
+// assign function show fox when button two clicked
+ buttonTwo.onclick = showFox;
